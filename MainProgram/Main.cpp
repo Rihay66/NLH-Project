809a0d4bc183add0e5b371559e05298d461bfa5c
Rihay66 <noiralter0660@gmail.com>
2022-05-30 13:18:36 -0700

    Added comments to code for better understanding and changed and fixed the error messages from ArduinoCheck.cpp

--- MainProgram/Main.cpp
+++ MainProgram/Main.cpp
@@ -2,7 +2,7 @@
 #include "inc/ArduinoCheck.h"
 
 string rgbinput(){
-	//Input from user
+	//Input from user in string form
 	cout<< ">> ";
 	string inputRead;
 	cin >> inputRead;
@@ -12,15 +12,20 @@ string rgbinput(){
 
 int main(){
 
+	//Initialize arduino check as a pointer
 	ArduinoCheck* ar = new ArduinoCheck;
+	//Initialize local com port to be set to whichever port was valid
 	const char* com_port;
 
+	//Use the class to cycle all COM ports
 	com_port = ar->boardCheck();
 
 	if(com_port != nullptr){
 		cout << "Arduino is ready to initialize" << endl;
+		//Free memory by deleting the arduino check class
 		delete ar;
 
+		//Initialize the connection with arduino board
 		SerialPort serial(com_port, 9600);
 
 		cout << "Arduino board initialized succesfully" << endl;
@@ -28,9 +33,11 @@ int main(){
 		while(serial.is_connected){
 			string input = rgbinput();
 
+			//Optional user input to exit the program
 			if(input == "sysexit")
 				return 0;
-			//translate
+
+			//translate into serial bytes
 			char* charArray = new char[input.size() + 1];
 			copy(input.begin(), input.end(), charArray);
 			charArray[input.size()] = '\n';
@@ -41,7 +48,8 @@ int main(){
 				cout << "Error: no input sent" << endl;
 				return 1;
 			}
-
+			
+			Sleep(1);
 		}
 	}else{
 		cout << "Error: No Arduino board found" << endl;
